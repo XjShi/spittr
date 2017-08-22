ac9bb5a01e3811a9bb70b10ea3b16973b54ff52b
xjshi <shixj.cs@foxmail.com>
2017-08-22 20:37:45 +0800

    using properties to place database configuration.

--- src/main/java/com/spittr/config/DataSourceConfig.java
+++ src/main/java/com/spittr/config/DataSourceConfig.java
@@ -1,20 +1,25 @@
 package com.spittr.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class DataSourceConfig {
+    @Autowired
+    org.springframework.core.env.Environment env;
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/spittr?useUnicode=true&characterEncoding=utf8");
-        dataSource.setUsername("root");
-        dataSource.setPassword("mysql2017");
+        dataSource.setDriverClassName(env.getProperty("database.driver"));
+        dataSource.setUrl(env.getProperty("database.url"));
+        dataSource.setUsername(env.getProperty("database.username"));
+        dataSource.setPassword(env.getProperty("database.password"));
         return dataSource;
     }
 }
